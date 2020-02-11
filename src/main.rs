73c2ab1c1509627b2d3411ef3041eb9d2e5086f3
conblem <mail@conblem.me>
2020-02-11 11:47:44 +0100

    Unused imports

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::str::FromStr;
 use std::error::Error;
 
 use tokio;
@@ -6,15 +5,13 @@ use tokio::runtime::Runtime;
 use trust_dns_server::store::forwarder::{ForwardAuthority, ForwardConfig};
 use trust_dns_server::proto::rr::Name;
 use trust_dns_resolver::config::NameServerConfigGroup;
-use trust_dns_server::authority::{ZoneType, Authority, LookupObject, AuthorityObject, Catalog};
-use trust_dns_resolver::proto::rr::RecordType;
-use trust_dns_client::rr::LowerName;
+use trust_dns_server::authority::{ZoneType, Catalog};
 use trust_dns_server::server::ServerFuture;
 use std::net::UdpSocket;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let mut runtime = Runtime::new().expect("failed to create Tokio Runtime");
+    let runtime = Runtime::new().expect("failed to create Tokio Runtime");
 
     let config = ForwardConfig {
         name_servers: NameServerConfigGroup::cloudflare_tls(),
